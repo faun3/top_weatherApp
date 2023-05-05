140fa3d68b291af7e7d99a7626c37bf5f45bd112
faun3 <antonioculcea@gmail.com>
2023-05-05 22:05:08 +0300

    make api call to weatherapi and log returned json

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("hello world");
+
+function makeRequest() {
+  fetch(
+    "http://api.weatherapi.com/v1/current.json?key=ed6348fefce647298a8184902230505&q=London"
+    //{ mode: "cors" }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+    });
+  //return weatherJSON;
+}
+
+makeRequest();
