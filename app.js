9db03643bc09a1aa1c1f1cfc9e4c81f32bb1eb88
faun3 <antonioculcea@gmail.com>
2023-05-05 23:26:52 +0300

    factor out IIFE in preparation of writing the DOM functions for the app

--- app.js
+++ app.js
@@ -1,31 +1,16 @@
 //TODO
 
-//makeRequest function should take in a string parameter that specifies the location
-//  we want to get weather data for
-//
-//makeRequest should handle getting a bad location from the users
-
-//write a parseRequest function that takes in the relevant data we need
-//  temp in C
-//  temp in F
-//  "description" string (i.e. cloudy, sunny, etc)
-//
-//this function should return an object with this relevant data
-
 //write out DOM interacting functions that take relevant data in
 //  app functions may need to return some values that tell these DOM functions
 //  if/what they need to display
 
-const getLocation = (() => {
-  //const formSubmit = document.getElementById("submitButton");
-  const form = document.getElementById("form");
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const locationInput = document.getElementById("location");
-    let requestedLocation = locationInput.value;
-    makeRequest(requestedLocation);
-  });
-})();
+const form = document.getElementById("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const locationInput = document.getElementById("location");
+  let requestedLocation = locationInput.value;
+  makeRequest(requestedLocation);
+});
 
 function makeRequest(location) {
   fetch(
@@ -33,7 +18,11 @@ function makeRequest(location) {
     //{ mode: "cors" }
   )
     .then(function (response) {
-      return response.json();
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error("API Call failed!");
+      }
     })
     .then(function (response) {
       let extractedData = {
