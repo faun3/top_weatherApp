a249c0c5c9053f13a4063af2f090fff7c057fd99
faun3 <antonioculcea@gmail.com>
2023-05-05 23:59:23 +0300

    write out displaying object

--- DOMstuff.js
+++ DOMstuff.js
@@ -11,3 +11,69 @@ form.addEventListener("submit", (event) => {
   let requestedLocation = locationInput.value;
   makeRequest(requestedLocation);
 });
+
+//we will display an icon (taken from weatherAPI) to tell users what weather to expect
+//  below that we will have the temperature displayed in big text
+//  next to the temp display will be a smaller exponent style text that displays
+//      the two toggles between Celsius and Farenheit temps
+
+const appendPoint = document.querySelector("weatherData");
+
+const weatherViewFactory = (appendPoint) => {
+  const resetDisplay = () => {
+    appendPoint.replaceChildren();
+  };
+
+  const generateSkeleton = () => {};
+
+  const getIcon = () => {};
+
+  const tempToggle = () => {
+    const weatherWrapperDiv = document.createElement("div");
+    weatherWrapperDiv.setAttribute("class", "weatherWrapperDiv");
+
+    const iconDiv = document.createElement("div");
+    iconDiv.setAttribute("class", "iconDiv");
+
+    //png sourced from weatherAPI
+    const iconSource = document.createAttribute("img");
+    //placeholder below, change returned object code to make it dynamic
+    iconSource.src = "https://cdn.weatherapi.com/weather/64x64/day/116.png";
+
+    const tempToggleDiv = document.createElement("div");
+    tempToggleDiv.setAttribute("class", "tempToggleDiv");
+
+    const celsiusDisplayText = document.createElement("p");
+    celsiusDisplayText.setAttribute("id", "cText");
+    celsiusDisplayText.setAttribute("data-toggled", "true");
+    celsiusDisplayText.textContent = "℃";
+
+    celsiusDisplayText.addEventListener("click", () => {
+      //swap currently active text to this one
+      //  active temperature is rendered with darker text
+    });
+
+    const pipeDivider = document.createElement("div");
+    pipeDivider.textContent = "|";
+
+    const farenheitDisplayText = document.createElement("p");
+    farenheitDisplayText.setAttribute("id", "fText");
+    farenheitDisplayText.setAttribute("data-toggled", "false");
+    farenheitDisplayText.textContent = "℉";
+
+    const bigTextDiv = document.createElement("div");
+    bigTextDiv.setAttribute("class", "bigTextDiv");
+
+    const bigTempText = document.createElement("p");
+    bigTempText.textContent =
+      requestedLocation.appendPoint.appendChild(weatherWrapperDiv);
+  };
+
+  const displayWeather = () => {
+    //call all required display functions
+  };
+  return { displayWeather };
+};
+
+let weatherView = weatherViewFactory(appendPoint);
+weatherView.displayWeather();
